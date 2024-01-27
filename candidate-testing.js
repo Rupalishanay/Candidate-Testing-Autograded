2b0cbcf63b48fb5187b51113ced839786c284d3f
Rupali Pakhale <rupalipakhale@rupalis-mbp.mynetworksettings.com>
2024-01-27 14:52:26 -0500

    multiques

--- candidate-testing.js
+++ candidate-testing.js
@@ -6,44 +6,51 @@ const input = require('readline-sync');
 let candidateName=" ";
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question=("Who was the first American woman in space? ");
-let correctAnswer="Sally Ride";
-let candidateAnswer="";
+let question="Who was the first American woman in space? ";
+let correctAnswer="Sally Ride", 
+  candidateAnswer=" ";
 
-//TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+//TODO: Variables for Part  
+let questions=["Who was the first American woman in space? ","True or false: 5 kilometer == 5000 meters? ","(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
+let correctAnswers=["Sally Ride", "True", "40", "Trajectory", 5];
+let candidateAnswers=[];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  Name=input.question("Enter the user name");
+  candidateName=input.question("Enter the user name");
 
    console.log("successfully user created");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+  
+  console.log(String(candidateAnswer));
+  for (let i = 0; i < questions.length; i++) {
+     candidateAnswers[i] = input.question(questions[i] + "\n");
+     
+  }
 
-candidateAnswer=input.question("Who was the first American woman in space? ");
-console.log(String(candidateAnswer));
 return candidateAnswer;
+
 }
 
 function gradeQuiz(candidateAnswer) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-  if(candidateAnswer==correctAnswer)
-{
- console.log("your answer is correct ");
-}
-else
-{
- console.log("answer is wrong");
-}
-
+  console.log(String(candidateAnswer));
+  
+    
+   if(candidateAnswer.toLowerCase===correctAnswer.toLowerCase())
+  {
+     console.log("your answer is correct ");
+  }
+ else
+ {
+    console.log("answer is wrong");
+ }
+  
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
 
@@ -54,7 +61,7 @@ function runProgram()
 {
   askForName();
     // TODO 1.1c: Greet candidate using their name //
-   console.log("Rupali welcome into the coding world ");
+    console.log(`Welcome to the coding world,${candidateName}!`);
   askQuestion();
   gradeQuiz(this.candidateAnswer);
 }
@@ -66,8 +73,10 @@ module.exports = {
   correctAnswer: correctAnswer,
   candidateAnswer: candidateAnswer,
   questions: questions,
-  correctAnswers: correctAnswers,
-  candidateAnswers: candidateAnswers,
+correctAnswers: correctAnswers,
+candidateAnswers: candidateAnswers,
+  
+  
   gradeQuiz: gradeQuiz,
   runProgram: runProgram
 };
