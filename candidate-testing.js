909cff3c3198cbd69988190c62a06be90a0bce17
Rupali Pakhale <rupalipakhale@rupalis-mbp.mynetworksettings.com>
2024-01-28 19:08:59 -0500

    quizz

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,12 +8,12 @@ let candidateName=" ";
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question="Who was the first American woman in space? ";
-let correctAnswer="Sally Ride", 
-  candidateAnswer=" ";
+let correctAnswer="Sally Ride"; 
+ let candidateAnswer=" ";
 
 //TODO: Variables for Part  
 let questions=["Who was the first American woman in space? ","True or false: 5 kilometer == 5000 meters? ","(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
-let correctAnswers=["Sally Ride", "True", "40", "Trajectory", 5];
+let correctAnswers=["Sally Ride", "true", "40", "Trajectory", "5"];
 let candidateAnswers=[];
 
 
@@ -47,7 +47,7 @@ function gradeQuiz(candidateAnswers) {
    let count=0;
     for(let i = 0 ; i < correctAnswers.length;i++){
       
-       if(candidateAnswers[i]===correctAnswers[i])
+       if(candidateAnswers[i].toLowerCase()===correctAnswers[i].toLowerCase())
    {
     count++;   
    }
